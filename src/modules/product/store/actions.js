0627b68db865baeff6f78f37afb1b8c44882bd01
Tefoh <tefo.ha27@gmal.com>
2021-04-24 23:37:41 +0430

    change api call

--- src/modules/product/store/actions.js
+++ src/modules/product/store/actions.js
@@ -3,7 +3,7 @@ import { SET_PRODUCTS_MUTATIONS } from './types'
 
 export default {
   async getProducts (context, { text }) {
-    const { data } = await axios.get('https://gist.githubusercontent.com/Tefoh/57a0ef76ab63a974105b9f0fbcb8475b/raw/d49e3d8104992ff6cc6742fbe91b0c642287837a/products.json')
+    const { data } = await axios.get('https://gist.githubusercontent.com/Tefoh/57a0ef76ab63a974105b9f0fbcb8475b/raw/49357fb98c1c6dde9070fd1a7ec1e5d532c43141/products.json')
     
     context.commit(SET_PRODUCTS_MUTATIONS, data)
 
